67a3bb2c15a0f1a428d5fc43a9c5f898e4bb814b
AuthenticCode313 <80369849+AuthenticCode313@users.noreply.github.com>
2021-04-15 08:39:50 -0700

    Added Waffles

--- BranchTest/Program.cs
+++ BranchTest/Program.cs
@@ -8,6 +8,7 @@ namespace BranchTest
         {
             Console.WriteLine("Hello Alice, Today is such a beautiful day!");
             Console.WriteLine("God is Good.");
+            Console.WriteLine("Waffles and Maple Syrup is good too");
         }
     }
 }
